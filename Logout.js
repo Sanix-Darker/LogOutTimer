ae6b5c388bf1a3d9f9033d7b58225ae5963b2865
Sanix Darker <sanix000@yahoo.fr>
2017-09-21 15:48:56 +0200

    Second Initial Commit

--- Logout.js
+++ Logout.js
@@ -1,28 +1,26 @@
-	var stop = false;
-	var initial_timer = 6000*15;
-	var timer = initial_timer;
-	var logoutUrl = 'unlock.html'; // URL to logout page.
+// LogOutTimer By Sanix Darker
 
-	function logOut(){
-		console.log("Aucune action...")
-		window.location = logoutUrl;
-	}
+var stop = false;
+var initial_timer = (60000*15);
+var timer = initial_timer;
+var logoutUrl = 'unlock.html?last_page_visited='+window.location.pathname ; // URL to logout page.
 
-	if(!stop){
-		setInterval(function(){
-			timer-=1000;
-			console.log("Timer:"+timer);
-			if(timer==0 || timer<0){
-				console.log("Deconnexion....");
-				logOut();
-				stop = true;
-			}
-		}, 1000);
-	}
+function logOut(){
+	window.location = logoutUrl;
+}
 
-	document.body.bind('click dblclick mousedown mouseenter mouseleave keyup mouseover',
-	function(e){
-		timer = initial_timer;
-		 console.log("Timer:"+timer);
-		 console.log("EventName:"+e.type);
-	});
+if(!stop){
+	setInterval(function(){
+		timer-=1000;
+		console.log("Timer:"+timer);
+		if(timer==0 || timer<0){
+			logOut()
+			stop = true;
+		}
+	}, 1000);
+}
+
+$('body').bind('click dblclick mousedown mouseenter mouseleave keyup mouseover',
+function(e){
+	timer = initial_timer;
+});
